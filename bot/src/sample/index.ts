7e1a721aac81885cd788de930ef0c0815e8fab6e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-16 17:08:47 +0900

    Lambda関数が正常に動作するようにした

--- bot/src/sample/index.ts
+++ bot/src/sample/index.ts
@@ -1,3 +1,14 @@
-export const slackHandler = async () => {
+import { APIGatewayEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
+
+/**
+ * Entry Point
+ */
+export const slackHandler = async (): Promise<APIGatewayProxyResult> => {
   console.log("Test");
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "OK",
+    })
+  };
 }
